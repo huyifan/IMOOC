ce7f25b6154c83312d2779d543cc0bd369257b0c
475838004@qq.com <475838004@qq.com>
2018-08-21 15:14:27 +0800

    移动端web适配

---
+++ 移动端web开发适配/code/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by hugo on 2018/8/21.
+ */
+require("./index.scss");
+
+let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+let htmlDOM = document.getElementsByTagName('html')[0];
+htmlDOM.style.fontSize = htmlWidth / 10 + 'px';
+
+window.addEventListener('resize', (e) => {
+  let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+
+  htmlDOM.style.fontSize = htmlWidth / 10 + 'px';
+
+})
