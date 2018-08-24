1ef4d1f83c84c90fed005db3f66456b4de4d2bd3
475838004@qq.com <475838004@qq.com>
2018-08-24 17:38:05 +0800

    初始化koa2项目-前端页面

--- 实战/Koa2框架从开始构建电影预告网站/代码/project/server/index.js
+++ 实战/Koa2框架从开始构建电影预告网站/代码/project/server/index.js
@@ -4,13 +4,23 @@
 //创建服务
 const Koa = require('koa')
 const app = new Koa()
+const views = require('koa-views') //视图解析
+const {resolve} = require('path')
+
 
 //1.问题1-new Koa时做了什么事情？
 //2.问题2-app.use的时候做了什么事情？
 //3.问题3-app.listen时候做了什么事情？
 
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
+
 app.use(async (ctx, next) => {
-  ctx.body = '电影首页'
+  await ctx.render('index', {
+    name: 'hugo',
+    age: '24'
+  })
 })
 
 //监听端口
